7e3d263eb7585180922b2bb8429e7a47241be566
PravinLengare <lengarepravin115@gmail.com>
2026-02-23 23:58:11 +0530

    prefix sum problem solved

--- Array/src/array/PrefixSum.java
+++ Array/src/array/PrefixSum.java
@@ -11,7 +11,7 @@ public class PrefixSum {
     public static int[] prefixSum(int[] arr){
         int n = arr.length;
         int []pref = new int[n+1];
-        pref[0] = arr[0];
+        pref[0] = 0;
 
         for (int i = 1; i <= n; i++) {
             pref[i] = pref[i-1] + arr[i-1];
@@ -70,11 +70,12 @@ public class PrefixSum {
 
 
         int[] ans = prefixSum(arr);
+        printArray(ans);
 
 
 
         //int []prefixSum = prefixSumWithoutMemory(arr);
-
+//
         System.out.println("Enter the no of queries : ");
         int q = scanner.nextInt();
         int sum = 0;
